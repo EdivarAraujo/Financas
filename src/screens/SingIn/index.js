f2813ce3c1e69e33c7d5f3a9af4bd8db7496db42
EdivarAraujo <edivar23araujo@gmail.com>
2023-02-04 23:56:20 -0300

    telaSignIn

--- src/screens/SingIn/index.js
+++ src/screens/SingIn/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import * as C from './styles'
+import {StatusBar} from 'react-native'
+import logo from '../../assets/logoSignIn.png'
+
+
 
 export default function SingIn(){
 
   return ( 
     <C.Container>
-      <C.TextTela>SingIn</C.TextTela>
+      <StatusBar translucent={true} backgroundColor='transparent'/>
+      <C.BackgroundImage source={logo}>
+
+      </C.BackgroundImage>
     </C.Container>
   )
 
